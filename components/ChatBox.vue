3e91b7ac923a510f3265c43540d5f40eed92d4e2
silasfelinus <silasfelinus@gmail.com>
2023-05-03 08:37:06 -0700

    chat bubble

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,89 +1,137 @@
-<template>
-  <div class="chat-box">
-    <div class="message-list" ref="messageList">
-      <div v-for="message in messages" :key="message.id" :class="messageClass(message)">
-        <img :src="userById(message.userId).avatar" alt="Avatar" />
-        <div class="message-content">
-          <strong>{{ userById(message.userId).name }}</strong>
-          <div v-html="renderMarkdown(message.text)" class="message-text"></div>
-          <span class="message-timestamp">{{ formatRelativeTime(message.createdAt) }}</span>
-        </div>
-      </div>
-      <div v-for="user in usersTyping" :key="user.id" class="message-received">
-        <img :src="user.avatar" alt="Avatar" />
-        <div class="message-content">
-          <strong>{{ user.name }}</strong>
-          <div class="typing-indicator"><div></div><div></div><div></div></div>
-        </div>
-      </div>
-    </div>
-    <div class="input-wrapper">
-      <input v-model="newMessage" @keydown.enter="sendMessage" placeholder="Type your message..." />
-      <button @click="sendMessage">Send</button>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { ref, watch, onMounted, nextTick } from "vue";
-import { Message, User } from "@/types";
-import { useMarkdown } from "@/composables/useMarkdown";
-import { useRelativeTime } from "@/composables/useRelativeTime";
+import { nanoid } from "nanoid";
+import { Message, User } from "~~/types";
+const props = withDefaults(
+  defineProps<{
+    messages: Message[];
+    users: User[];
+    me: User;
+    usersTyping?: User[];
+  }>(),
+  {
+    usersTyping: () => [],
+  }
+);
 
-interface Props {
-  messages: Message[];
-  users: User[];
-  me: User;
-  usersTyping?: User[];
+defineEmits<{
+  (e: "newMessage", payload: Message): void;
+}>();
+
+const open = ref(false);
+
+function getUser(id: string) {
+  return props.users.find((user) => user.id === id);
 }
 
-const props = defineProps<Props>({
-  messages: Array as unknown as () => Message[],
-  users: Array as unknown as () => User[],
-  me: Object as unknown as () => User,
-  usersTyping: { type: Array as unknown as () => User[], default: () => [] },
+// focus input whenever chatbox is opened
+const input = ref();
+watch(open, () => {
+  if (!open.value) return;
+  nextTick(() => {
+    (input.value as HTMLInputElement).focus();
+  });
 });
 
-const emit = defineEmit(["new-message"]);
-
-const newMessage = ref("");
-const messageList = ref<HTMLElement | null>(null);
-const { renderMarkdown } = useMarkdown();
-const { formatRelativeTime } = useRelativeTime();
-
+// keep messages anchored to bottom
+const messageBox = ref();
 watch(
-  () => [newMessage.value, messageList.value],
-  async () => {
-    await nextTick();
-    if (messageList.value) {
-      messageList.value.scrollTop = messageList.value.scrollHeight;
-    }
+  () => props.messages,
+  () => {
+    nextTick(
+      () => (messageBox.value.scrollTop = messageBox.value.scrollHeight)
+    );
   },
   { deep: true }
 );
-
-onMounted(() => {
-  if (messageList.value) {
-    messageList.value.scrollTop = messageList.value.scrollHeight;
-  }
-});
-
-function userById(id: string) {
-  return props.users.find((user) => user.id === id) || { name: "Unknown", avatar: "/default-avatar.jpg" };
-}
-
-function messageClass(message: Message) {
-  return { "message-sent": message.userId === props.me.id, "message-received": message.userId !== props.me.id };
-}
-
-function sendMessage() {
-  if (newMessage.value.trim() === "") return;
-  emit("new-message", {
-    text: newMessage.value.trim(),
-    id: Date.now().toString(),
-    userId: props.me.id,
-    createdAt: new Date(),
-  });
-  newMessage.value = "";
-}
 </script>
+<template>
+  <div class="fixed bottom-[10px] right-[10px]">
+    <button
+      v-show="!open"
+      @click="open = true"
+      class="bg-blue-500 p-3 rounded"
+      data-test="chat-widget-trigger"
+    > 
+      <IconChat class="h-8 w-8 text-white" />
+    </button>
+    <div
+      v-if="open"
+      data-test="chat-widget-content"
+      class="box bg-gray-300 dark:bg-gray-800 rounded w-[450px] overflow-hidden"
+    >
+      <!-- Header -->
+      <header
+        class="dark:bg-gray-900 bg-gray-200 px-4 flex justify-between items-center"
+      >
+        Serendipity Digital Assistant
+        <button class="p-4 pr-0" @click="open = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </header>
+      <!-- Messages -->
+      <div class="messages p-4 overflow-y-scroll max-h-[80vh]" ref="messageBox">
+        <div v-if="!messages.length" class="text-center w-[350px] m-auto">
+          <strong class="text-lg">Hi, I'm Serendipity!</strong>
+          <p>Your next-generation digital assistant</p>
+          <strong class="block mt-10">Go ahead and ask me something:</strong>
+          <ul class="list-inside list-disc text-left">
+            <li>Who are you?</li>
+            <li>What is Cafe Purr?</li>
+            <li>How was this tool built?</li>
+          </ul>
+        </div>
+        <ChatBubble
+          data-test="chat-bubble"
+          v-for="message in messages"
+          :key="message.id"
+          :message="message"
+          :user="getUser(message.userId)"
+          :my-message="message.userId === me.id"
+        />
+
+        <ChatBubble v-for="user in usersTyping" :key="user.id" :user="user">
+          <AppLoading />
+        </ChatBubble>
+      </div>
+      <!-- Footer -->
+      <footer class="p-2">
+        <input
+          data-test="chat-input"
+          ref="input"
+          class="input w-full px-2 block"
+          type="text"
+          placeholder="Type your message"
+          @keypress.enter="
+            $emit('newMessage', {
+              id: nanoid(),
+              userId: me.id,
+              createdAt: new Date(),
+              text: ($event.target as HTMLInputElement).value,
+            });
+            ($event.target as HTMLInputElement).value = '';
+          "
+        />
+
+        <div class="h-6 py-1 px-2 text-sm italic">
+          <span v-if="usersTyping.length">
+            {{ usersTyping.map((user) => user.name).join(" and ") }}
+            {{ usersTyping.length === 1 ? "is" : "are" }} typing
+          </span>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
---
+++ components/ChatBubble.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { Message, User } from '../types'
+
+const props = defineProps < {
+    user?: User
+    message: Message
+}>()
+
+const relativeTime = 
+</script>
+
+<template>
+    <div class="chat">
+        <div class="chat-image avatar"><img src="user?.avatar" /></div>
+        <div class="chat-header"></div>
+        <div class="chat-bubble bg-white dark:bg-gray-900 max-w-max w-full"></div>
+        {{  message.text  }}
+    </div>
+</template>
