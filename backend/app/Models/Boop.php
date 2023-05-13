3fb2a143e95e97d65781c0c0bf03494a12bcaffe
silasfelinus <silasfelinus@gmail.com>
2023-05-12 18:23:54 -0700

    added backend

---
+++ backend/app/Models/Boop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Boop extends Model
+{
+    use HasFactory;
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   runtimeConfig: {
+    public: {
+      appURL: "https://cafepurr.com",
+    },
     OPENAI_API_KEY: process.env.NUXT_OPENAI_API_KEY || "",
   },
   ssr: false,
